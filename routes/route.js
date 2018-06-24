54e27bcfb813ab373188591e0abb4f060ab2b233
Kevin Lai <kevin.km.lai@gmail.com>
2018-06-25 09:33:54 +1000

    first commit

---
+++ routes/route.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+
+//Require the controllers WHICH WE DID NOT CREATE YET!
+const product_controller = require("../controllers/product.controller");
