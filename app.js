32fbfd888f0ebc4e24acb6f0b9f6de41b5195206
Kevin Lai <kevin.km.lai@gmail.com>
2018-06-25 11:03:49 +1000

    added controllers

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
-//initialize our express app
+// import routes for the products
+const product = require("./routes/product.route");
+// initialize our express app
 const app = express();
-
+app.use("/products", product);
 let port = 1234;
 
 app.listen(port, () => {
--- routes/product.route.js
+++ routes/product.route.js
@@ -3,3 +3,8 @@ const router = express.Router();
 
 //Require the controllers WHICH WE DID NOT CREATE YET!
 const product_controller = require("../controllers/product.controller");
+
+// this is just a test url to make sure all of our files are communicate correctly
+router.get("./test", product_controller.test);
+
+module.exports = router;
